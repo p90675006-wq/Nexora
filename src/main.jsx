69d7556284cece2ee1e4b78deb8a84ff89f1d28a
p90675006-wq <p90675006@gmail.com>
2026-08-09 00:16:01 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Studymate">
       <OnboardingProvider>
         <App />
       </OnboardingProvider>
